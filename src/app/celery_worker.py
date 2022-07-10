247271d64a4029fd08e485e6a05a591d92fa637c
solegray4 <t.t.songca@gmail.com>
2022-07-10 11:30:31 +0200

    none: change task input

--- src/app/celery_worker.py
+++ src/app/celery_worker.py
@@ -1,12 +1,10 @@
-import os
-import time
 from PIL import Image
 
 from celery import Celery
 from dotenv import load_dotenv
 from app.core.config import CELERY_BACKEND, CELERY_BROKER
 
-from app.core.files import PATH_FILES_ORIGINAL, PATH_FILES_RESIZED
+from app.files import PATH_FILES_ORIGINAL, PATH_FILES_RESIZED
 
 celery = Celery(__name__)
 celery.conf.broker_url = CELERY_BROKER
@@ -16,13 +14,14 @@ celery.conf.update(
 )
 
 @celery.task(name="resize_image_task")
-def resize_image_task(filename):
+def resize_image_task(image):
     size = {
         "width": 100,
         "height": 100
         }
     size_defined = size['width'], size['height']
-    img = Image.open(f"{PATH_FILES_ORIGINAL}{filename}", mode="r")
+    filename = image['filename']
+    img = Image.open(PATH_FILES_ORIGINAL/ filename, mode="r")
     img.thumbnail(size_defined)
-    img.save(PATH_FILES_RESIZED + filename)
+    img.save(PATH_FILES_RESIZED / filename)
     return  {'filename': filename, "image_size": str(img.size)}
